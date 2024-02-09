be611112421870f40dd3af9eea21b4690039c869
Mohamed <mohahamdy11@gmail.com>
2024-02-09 18:25:54 +0200

    adding new page about with css and js make modifing in contact page and index

---
+++ script/about.js
@@ -0,0 +1,29 @@
+/** ----------- Navbar ----------- */
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const close = document.getElementById("close");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
+
+/** ----------- Scroll To Top ----------- */
+function scrollToTop() {
+  document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("scrollToTopBtn").style.display = "none";
+  }
+};
